c358b6d3724da6bd96510adda61489c519201ffd
Lindon779 <906861749@qq.com>
2020-10-08 10:20:01 +0800

    HRP_Test

---
+++ HRP.Common/src/main/java/com/gzhh/hrp/db/dao/TestDao.java
@@ -0,0 +1,7 @@
+package com.gzhh.hrp.db.dao;
+
+public class TestDao {
+    public String Daotest(String str) {
+        return str;
+    }
+}
---
+++ HRP.Common/src/main/java/com/gzhh/hrp/db/service/ServerTest.java
@@ -0,0 +1,9 @@
+package com.gzhh.hrp.db.service;
+
+public interface ServerTest {
+    public void userGroupSetting();
+    public void addUser();
+    public void deleteUser();
+    public void updateUser();
+    public void updatePassword();
+}
